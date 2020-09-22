1ec4acd6c289e95a34d56ba8643c68702d88679b
Oriol Arcas <oriol.arcas@gmail.com>
2020-09-22 19:52:05 +0200

    Initial commit

---
+++ alvaromusical/__main__.py
@@ -0,0 +1,25 @@
+import random
+import time
+import tweepy
+import yaml
+
+def tweet(conf, text):
+    auth = tweepy.OAuthHandler(conf["CONSUMER_KEY"], conf["CONSUMER_SECRET"])
+    auth.set_access_token(conf["ACCESS_KEY"], conf["ACCESS_SECRET"])
+    api = tweepy.API(auth)
+    
+    if len(text) <= 280:
+        api.update_status(status=text)
+
+if __name__ == '__main__':
+    with open('conf.yaml') as conf_file:
+        conf = yaml.load(conf_file, Loader=yaml.FullLoader)
+        
+    with open('musica.yaml') as tweets_file:
+        tweets = yaml.load(tweets_file, Loader=yaml.FullLoader)
+        random.seed()
+        t = random.choice(tweets)
+        print("Publicant:")
+        print(t)
+        tweet(conf, t)
+
